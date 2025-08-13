b698076df38567b92b893fae49832eb34612d87f
aqkey <warmtune-unit@outlook.jp>
2025-08-14 01:07:32 +0900

    modify E2E test sorcerer

--- tests/sorcerer.spec.ts
+++ tests/sorcerer.spec.ts
@@ -55,7 +55,7 @@ test('first player draws a magician and eliminates opponent holding princess', a
       roomId: roomName,
       deck: [1, 1, 1, 5], // draw order: 魔術師, 兵士, 兵士, 兵士
       hands: {
-        [aliceId]: [1], // 兵士
+        [aliceId]: [4], // 僧侶
         [bobId]: [8],   // 姫
       },
     },
@@ -66,24 +66,27 @@ test('first player draws a magician and eliminates opponent holding princess', a
 
   // Player1 plays 魔術師 targeting bob
   await page1.getByRole('button', { name: '魔術師' }).click();
-  await page1.getByRole('button', { name: 'bob' }).click();
+  await page1.getByLabel('bob').check();
+  await sleep(1000);
+  await page1.getByRole('button', { name: '決定' }).click();
 
   // bob should be eliminated for discarding 姫
-  await expect(page1.getByText('bob (脱落)')).toBeVisible();
+  await expect(page1.getByText('勝者: alice さん！')).toBeVisible();
 
   await context1.close();
   await context2.close();
 });
 
-test('first player draws a magician and second player do not use countess ', async ({ browser }) => {
 
-  
+// E2E scenario: deck [兵士,兵士,騎士,兵士],
+// initial hands: Player1 道化, Player2 僧侶,
+// first player draws 兵士 and eliminates opponent.
+
+test('first player draws a magician and use.  second player do not discart countess ', async ({ browser }) => {
   const context1: BrowserContext = await browser.newContext();
   const page1: Page = await context1.newPage();
   const context2: BrowserContext = await browser.newContext();
   const page2: Page = await context2.newPage();
-
-
 
   await page1.goto('http://localhost:3000');
   await page2.goto('http://localhost:3000');
@@ -125,7 +128,7 @@ test('first player draws a magician and second player do not use countess ', asy
       deck: [1, 1, 1, 5], // draw order: 魔術師, 兵士, 兵士, 兵士
       hands: {
         [aliceId]: [1], // 兵士
-        [bobId]: [10],   // 姫
+        [bobId]: [10],   // 伯爵夫人
       },
     },
   });
@@ -135,9 +138,18 @@ test('first player draws a magician and second player do not use countess ', asy
 
   // Player1 plays 魔術師 targeting bob
   await page1.getByRole('button', { name: '魔術師' }).click();
-  await page1.getByRole('button', { name: 'bob' }).click();
+  await page1.getByLabel('bob').check();
+  await sleep(1000);
+  await page1.getByRole('button', { name: '決定' }).click();
 
-  // bob should be skip and next turn
+  // bob should be eliminated for discarding 姫
+  await expect(page1.getByText('相手のターンです。お待ちください...')).toBeVisible();
+
+　// Player1 draws soldier
+  await page2.getByRole('button', { name: 'カードを引く' }).click();
+  // Player1 plays soldier targeting bob
+  await page2.getByRole('button', { name: '兵士' }).click();
+
 
 
   await context1.close();
